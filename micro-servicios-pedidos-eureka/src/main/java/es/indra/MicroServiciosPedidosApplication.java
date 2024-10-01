d9f832dcb9c7e83cc9acc3af42871e0662b42cad
Carballo Failde <aocarballo@indra.es>
2024-10-01 13:09:16 +0200

    modidicamos property

--- micro-servicios-pedidos-eureka/src/main/java/es/indra/MicroServiciosPedidosApplication.java
+++ micro-servicios-pedidos-eureka/src/main/java/es/indra/MicroServiciosPedidosApplication.java
@@ -3,12 +3,11 @@ package es.indra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name="servicios-productos")
+//@RibbonClient(name="servicios-productos")
 @EnableEurekaClient
 public class MicroServiciosPedidosApplication {
 
